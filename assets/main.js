0c4f94ee6dc6d38725871dfd95294f628f565b4b
Gus Mota <gustavo.milani22@gmail.com>
2022-06-11 11:59:51 -0300

    dataset and func to check if cards match

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,33 @@
 const cards = document.querySelectorAll('.card')
+let hasFlippedCard = false
+let firstCard, secondCard
+
 
 function flipCard() {
-    this.classList.toggle('flip')
+    this.classList.add('flip')
+    
+    if(!hasFlippedCard){
+        hasFlippedCard = true
+        firstCard = this
+        return
+    }
+    
+    secondCard = this
+    hasFlippedCard = false
+    checkForMatch()
+}
+
+function unflipCard() {
+
+}
+
+function checkForMatch() {
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards()
+        return
+    }
+
+    unflipCard()
 }
 
 cards.forEach((card) => {
