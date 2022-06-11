6dfb904382965fc2e339f04d79dc3e8af4e9530b
Gus Mota <gustavo.milani22@gmail.com>
2022-06-11 12:03:43 -0300

    Revert "dataset and func to check if cards match"
    This reverts commit 0c4f94ee6dc6d38725871dfd95294f628f565b4b.

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,7 @@
 const cards = document.querySelectorAll('.card')
-let hasFlippedCard = false
-let firstCard, secondCard
-
 
 function flipCard() {
-    this.classList.add('flip')
-    
-    if(!hasFlippedCard){
-        hasFlippedCard = true
-        firstCard = this
-        return
-    }
-    
-    secondCard = this
-    hasFlippedCard = false
-    checkForMatch()
-}
-
-function unflipCard() {
-
-}
-
-function checkForMatch() {
-    if(firstCard.dataset.card === secondCard.dataset.card){
-        disableCards()
-        return
-    }
-
-    unflipCard()
+    this.classList.toggle('flip')
 }
 
 cards.forEach((card) => {
